34b622432a3dc76003f7773da174106a8482d911
Luca Furlano <furlanoluca@libero.it>
2020-08-15 17:27:09 +0200

    improved error correction

--- src/execution/Corrector.java
+++ src/execution/Corrector.java
@@ -6,7 +6,12 @@ public class Corrector {
 
 	public static ArrayList<Double> adjust(ArrayList<Double> values, String name) {
 
-		int correctionCounter = 0;
+		ArrayList<Double> output = new ArrayList<Double>();
+		
+		int minorErrorCounter = 0;
+		int spikeCounter = 0;
+		
+		output.add(values.get(0));
 
 		for(int i = 1; i<=values.size()-2; i++) {
 
@@ -22,19 +27,29 @@ public class Corrector {
 			if(i >= 2) valueIm2 = values.get(i-2);
 			if(i >= 3) valueIm3 = values.get(i-3);
 
-			if(	(valueI < valueIm1 && valueI < valueIm2 && valueI < valueIm3) ||
-					(valueI > valueIp1 && valueI > valueIp2 && valueI > valueIp3) ) {
-				// error not acceptable - spike detected
-				valueI = (valueIm1 + valueIp1)/2;
-				values.set(i,valueI);
-				correctionCounter++;
+			if( (valueI < valueIm1 && valueI < valueIm2) ||
+					(valueI > valueIp1 && valueI > valueIp2) ) {
+				
+				if(valueI < valueIm3 || valueI > valueIp3) {
+					// error not acceptable - spike detected
+					valueI = (valueIm1 + valueIp1)/2.0;
+					spikeCounter++;
+				} else {
+					// minor error
+					valueI = (valueIm1 + valueIp1 + valueI)/3.0;
+					minorErrorCounter++;
+				}	
 			}
-
+			output.add(valueI);
 		}
+		
+		output.add(values.get(values.size()-1));
 
-		System.out.println("[" + name + "] adjusted wrong values: " + correctionCounter);
+		System.out.println("[" + name + "] adjusted wrong values: ");
+		System.out.println(" - minor errors: " + minorErrorCounter);
+		System.out.println(" - spike errors: " + spikeCounter);
 
-		return values;
+		return output;
 
 	}
 
