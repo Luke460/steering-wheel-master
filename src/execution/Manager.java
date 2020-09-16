d93577d510e10be1ef1ef8a57a7242f2321156a2
Luca Furlano <furlanoluca@libero.it>
2020-09-16 15:27:46 +0200

    csv file generation bug fix

--- src/execution/Manager.java
+++ src/execution/Manager.java
@@ -8,7 +8,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Locale;
