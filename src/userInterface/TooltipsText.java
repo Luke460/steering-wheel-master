17bb26750f4aaf538a766e7c00be49eba144e3d5
Luke460 <furlanoluca@libero.it>
2021-12-18 01:16:37 +0100

    minor: tooltips update

--- src/userInterface/TooltipsText.java
+++ src/userInterface/TooltipsText.java
@@ -4,16 +4,16 @@ public interface TooltipsText {
 	
 	//// Execution settings
 	
-	public static final String FILE_BROWSER_DESCRIPTION = "Choose your input calibration file. Refer to the documentation for more details.";
-	public static final String AGGREGATION_ORDER_DESCRIPTION = "Lower values of aggregation order makes your force feedback correction more precise, while higher values makes your force feedback smoother.";
-	public static final String AUTO_DESCRIPTION = "Basic settings for your wheel. Refer to the documentation page to find the best settings for your steering wheel.";
-	public static final String PEAK_REDUCTION_DESCRIPTION = "Increase to reduce the maximum amount of force sent to your steering wheel by introducing soft clipping.";
-	public static final String POWER_ENHANCEMENT_DESCRIPTION = "Increase to boost your wheel FFB strength. This option increases the low and medium values of FFB in a progressive manner by introducing soft clipping.";
-	public static final String DZ_ENHANCEMENT_DESCRIPTION = "Increase if you have vibrations in the central area of the steering wheel, decrease if you still have a FFB dead zone with the generated lut.";
-	public static final String LINEARIZE_NEAR_ZERO_DESCRIPTION = "Experimental feature: improve fidelity of low ffb values. Enabling this option may require you to slightly increase the dead zone enhancement value.";
-	public static final String LUT_METHOD_DESCRIPTION = "Advanced lut generation uses all the data provided by the csv file to attempt to correct the steering wheel reactions, while linear lut generation uses the csv file only to correct the dead zone.";
-	public static final String DONATION_DESCRIPTION = "Donations are not required, but always accepted with pleasure. Thanks for your support!";
-	public static final String CSV_SETTINGS_DESCRIPTION = "Configure how to read the input force feedback calibration file.";
-	public static final String PREVIEW_DESCRIPTION = "Displays force feedback and applied correction values ​​on a graph.";
-	public static final String GENERATE_LUT_DESCRIPTION = "Generate a lut file with the current settings.";
+	String FILE_BROWSER_DESCRIPTION = "Choose your input calibration file. Refer to the documentation for more details.";
+	String AGGREGATION_ORDER_DESCRIPTION = "Lower values of aggregation order makes your force feedback correction more precise, while higher values makes your force feedback smoother.";
+	String AUTO_DESCRIPTION = "Basic settings for your wheel. Refer to the documentation page to find the best settings for your steering wheel.";
+	String PEAK_REDUCTION_DESCRIPTION = "Increase to reduce the maximum amount of force sent to your steering wheel by introducing soft clipping.";
+	String POWER_ENHANCEMENT_DESCRIPTION = "Increase to boost your wheel FFB strength. This option increases the low and medium values of FFB in a progressive manner by introducing soft clipping.";
+	String DZ_ENHANCEMENT_DESCRIPTION = "Increase if you have vibrations in the central area of the steering wheel, decrease if you still have a FFB dead zone with the generated lut.";
+	String LINEARIZE_NEAR_ZERO_DESCRIPTION = "Experimental feature: improve fidelity of low ffb values. Enabling this option may require you to slightly increase the dead zone enhancement value.";
+	String LUT_METHOD_DESCRIPTION = "Advanced lut generation uses all the data provided by the csv file to attempt to correct the steering wheel reactions, while linear lut generation uses the csv file only to correct the dead zone.";
+	String DONATION_DESCRIPTION = "Donations are not required, but always accepted with pleasure. Thanks for your support!";
+	String CSV_SETTINGS_DESCRIPTION = "Configure how to read the input force feedback calibration file.";
+	String PREVIEW_DESCRIPTION = "Displays force feedback and applied correction values ​​on a graph.";
+	String GENERATE_LUT_DESCRIPTION = "Generate a lut file with the current settings.";
 }
